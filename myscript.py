a08593d5722166ad2e803cfe02e36e2c4baefe66
momo <73680098+YamonpornChertbamrung@users.noreply.github.com>
2023-03-03 21:33:40 +0700

    Update myscript.py

--- myscript.py
+++ myscript.py
@@ -2,7 +2,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("name", help="the name to greet")
-parser.add_argument("--age", help="the age of the person being greeted", type=int)
+parser.add_argument("age", help="the age of the person being greeted", type=int)
 args = parser.parse_args()
 
 greeting = "Hello, " + args.name + "!"
